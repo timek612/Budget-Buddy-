bf94ce05b792211cd102bf344eeff453322f6e3e
AJ Timek <Timek612@gmail.com>
2022-09-09 10:46:40 -0500

    retreival for recurring working. Working on adding new expenses

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,12 +1,126 @@
+import './NewExpense.css'
 import { useEffect } from 'react'
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function NewExpense () {
+    const history = useHistory()
+    const dispatch = useDispatch()
 
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('')
+    const [date, setDate] = useState('')
+    const [total, setTotal] = useState('')
+    const [type, setType] = useState('')
+
+    const dropDownChange = (e) => {
+        setCategory(e.target.value)
     
+    }
+
+    const typeChange = (e) => {
+        setType(e.target.value)
+    }
+
+    const handleAdd = () => {
+        console.log('added');
+        dispatch ({
+            type: 'NEW_EXPENSE',
+            payload: {
+                description,
+                category,
+                date,
+                total,
+                type
+            }
+        })
+        setDescription('')
+        setCategory('initial')
+        setDate('')
+        setTotal('')
+        setType('initial')
+    }
 
     return (
-        <h1>New Expense</h1>
+        <>
+            <h1 id='newExpenseHeader'>New Expense</h1>
+            <br />
+            <br />
+            <div className='newExpenseInputs'>
+
+                <input type="text" placeholder="Expense*"
+                    className='personalInput'
+                    value={description}
+                    required
+                    onChange={(event) => setDescription(event.target.value)}
+
+                />
+
+                <br />
+                <br />
+
+                <select name="category" className='personalInput' 
+                id='newExpenseDropDown'
+                value={category} onChange={dropDownChange}
+                >
+                    <option value="initial">Select a Category</option>
+                    <option value={1}>Housing</option>
+                    <option value={2}>Transportation</option>
+                    <option value={3}>Food</option>
+                    <option value={4}>Utilities</option>
+                    <option value={5}>Clothing</option>
+                    <option value={6}>Medical</option>
+                    <option value={7}>Insurance</option>
+                    <option value={8}>Household</option>
+                    <option value={9}>Lifestyle</option>
+                    <option value={10}>Debt</option>
+                    <option value={11}>Education</option>
+                    <option value={12}>Entertainment</option>
+                    <option value={13}>Donations</option>
+                </select>
+
+                <br />
+                <br />
+
+                <input type="date" placeholder="Date*"
+                    className='personalInput'
+                    value={date}
+                    required
+                    onChange={(event) => setDate(event.target.value)}
+
+                />
+
+                <br />
+                <br />
+
+                <input type="number" placeholder="Total Cost*"
+                    className='personalInput'
+                    value={total}
+                    required
+                    onChange={(event) => setTotal(event.target.value)}
+
+                />
+
+                <br />
+                <br />
+
+                <select name="recurringOrIndividual" className='personalInput' 
+                id='recurringOrIndividual'
+                value={type} onChange={typeChange}
+                >
+                    <option value="initial">Select a Type</option>
+                    <option value={true}>Recurring</option>
+                    <option value={false}>Individual</option>
+                   
+                </select>
+
+
+            </div>
+            <button id='plusButton' onClick={() => handleAdd()}>+</button>
+            <button id='recurringNextBtn' onClick={() => handleNext()}>Next</button>
+        </>
     )
 }
 
--- src/components/RecurringExpenses/RecurringExpenses.jsx
+++ src/components/RecurringExpenses/RecurringExpenses.jsx
@@ -1,11 +1,12 @@
 import { useHistory } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import './RecurringExpenses.css'
 function RecurringExpenses () {
 
     const history = useHistory()
     const dispatch = useDispatch()
-    const recurringExpenses = useSelector((store) => store.expenseReducer)
+    const recurringExpenses = useSelector((store) => store.expenseReducer.expenseReducer)
     console.log(recurringExpenses);
 
     useEffect(() => {
@@ -20,8 +21,19 @@ function RecurringExpenses () {
 
     return (
         <div>
-        <h1>RecurringExpenses</h1>
-        <button onClick={() => individualExpenses()}>Individual</button>
+        <h1 id='recurringHeader'>RecurringExpenses</h1>
+        <button id='btnToIndividual' onClick={() => individualExpenses()}>Individual</button>
+        <section id='list'>
+            {recurringExpenses.map(expense => {
+                return (
+                    <div key={expense.description} className='recurringExpenseDiv'>
+                        <p>{expense.date}</p>
+                        <p>{expense.description}</p>
+                        <p>{expense.cost}</p>
+                    </div>
+                )
+            })}
+        </section>
         </div>
     )
 }
